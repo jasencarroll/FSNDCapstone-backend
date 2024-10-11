b80eb0e34ea9093ea720c99e9c3fc37360beebee
Jasen Carroll <108644550+jasencarroll@users.noreply.github.com>
2024-10-11 18:04:34 -0400

    models and routes, need to setup flaskenv, db, etc.

--- app.py
+++ app.py
@@ -1,29 +1,178 @@
 import os
-from flask import Flask
-from models import setup_db
+from flask import Flask, request, jsonify, abort
+from flask_sqlalchemy import SQLAlchemy
+from models import setup_db, Movie, Actor
 from flask_cors import CORS
 
-def create_app(test_config=None):
+# Auth0 and RBAC imports
+from auth import requires_auth
 
+def create_app(test_config=None):
     app = Flask(__name__)
     setup_db(app)
     CORS(app)
 
-    @app.route('/')
-    def get_greeting():
-        excited = os.environ['EXCITED']
-        greeting = "Hello" 
-        if excited == 'true': 
-            greeting = greeting + "!!!!! You are doing great in this Udacity project."
-        return greeting
+    # GET /movies
+    @app.route('/movies', methods=['GET'])
+    @requires_auth('get:movies')
+    def get_movies(payload):
+        movies = Movie.query.all()
+        formatted_movies = [movie.format() for movie in movies]
+        return jsonify({
+            'success': True,
+            'movies': formatted_movies
+        }), 200
 
-    @app.route('/coolkids')
-    def be_cool():
-        return "Be cool, man, be coooool! You're almost a FSND grad!"
+    # GET /actors
+    @app.route('/actors', methods=['GET'])
+    @requires_auth('get:actors')
+    def get_actors(payload):
+        actors = Actor.query.all()
+        formatted_actors = [actor.format() for actor in actors]
+        return jsonify({
+            'success': True,
+            'actors': formatted_actors
+        }), 200
+
+    # POST /movies
+    @app.route('/movies', methods=['POST'])
+    @requires_auth('post:movies')
+    def create_movie(payload):
+        body = request.get_json()
+
+        title = body.get('title', None)
+        release_date = body.get('release_date', None)
+
+        if not title or not release_date:
+            abort(422)
+
+        try:
+            movie = Movie(title=title, release_date=release_date)
+            movie.insert()
+            return jsonify({
+                'success': True,
+                'movie': movie.format()
+            }), 201
+        except:
+            abort(500)
+
+    # POST /actors
+    @app.route('/actors', methods=['POST'])
+    @requires_auth('post:actors')
+    def create_actor(payload):
+        body = request.get_json()
+
+        name = body.get('name', None)
+        age = body.get('age', None)
+        gender = body.get('gender', None)
+
+        if not name or not age or not gender:
+            abort(422)
+
+        try:
+            actor = Actor(name=name, age=age, gender=gender)
+            actor.insert()
+            return jsonify({
+                'success': True,
+                'actor': actor.format()
+            }), 201
+        except:
+            abort(500)
+
+    # DELETE /movies/<int:movie_id>
+    @app.route('/movies/<int:movie_id>', methods=['DELETE'])
+    @requires_auth('delete:movies')
+    def delete_movie(payload, movie_id):
+        movie = Movie.query.get(movie_id)
+        if not movie:
+            abort(404)
+
+        try:
+            movie.delete()
+            return jsonify({
+                'success': True,
+                'deleted': movie_id
+            }), 200
+        except:
+            abort(500)
+
+    # DELETE /actors/<int:actor_id>
+    @app.route('/actors/<int:actor_id>', methods=['DELETE'])
+    @requires_auth('delete:actors')
+    def delete_actor(payload, actor_id):
+        actor = Actor.query.get(actor_id)
+        if not actor:
+            abort(404)
+
+        try:
+            actor.delete()
+            return jsonify({
+                'success': True,
+                'deleted': actor_id
+            }), 200
+        except:
+            abort(500)
+
+    # PATCH /movies/<int:movie_id>
+    @app.route('/movies/<int:movie_id>', methods=['PATCH'])
+    @requires_auth('patch:movies')
+    def update_movie(payload, movie_id):
+        movie = Movie.query.get(movie_id)
+        if not movie:
+            abort(404)
+
+        body = request.get_json()
+
+        title = body.get('title', None)
+        release_date = body.get('release_date', None)
+
+        if title:
+            movie.title = title
+        if release_date:
+            movie.release_date = release_date
+
+        try:
+            movie.update()
+            return jsonify({
+                'success': True,
+                'movie': movie.format()
+            }), 200
+        except:
+            abort(500)
+
+    # PATCH /actors/<int:actor_id>
+    @app.route('/actors/<int:actor_id>', methods=['PATCH'])
+    @requires_auth('patch:actors')
+    def update_actor(payload, actor_id):
+        actor = Actor.query.get(actor_id)
+        if not actor:
+            abort(404)
+
+        body = request.get_json()
+
+        name = body.get('name', None)
+        age = body.get('age', None)
+        gender = body.get('gender', None)
+
+        if name:
+            actor.name = name
+        if age:
+            actor.age = age
+        if gender:
+            actor.gender = gender
+
+        try:
+            actor.update()
+            return jsonify({
+                'success': True,
+                'actor': actor.format()
+            }), 200
+        except:
+            abort(500)
 
     return app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='localhost', port=5001)
--- models.py
+++ models.py
@@ -1,18 +1,14 @@
 import os
-from sqlalchemy import Column, String, create_engine
+from sqlalchemy import Column, String, Integer, Date, create_engine
 from flask_sqlalchemy import SQLAlchemy
-import json
 
-database_path = os.environ['DATABASE_URL']
+# Database setup
+database_path = os.environ.get('DATABASE_URL')
 if database_path.startswith("postgres://"):
-  database_path = database_path.replace("postgres://", "postgresql://", 1)
+    database_path = database_path.replace("postgres://", "postgresql://", 1)
 
 db = SQLAlchemy()
 
-'''
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
-'''
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -20,24 +16,43 @@ def setup_db(app, database_path=database_path):
     db.init_app(app)
     db.create_all()
 
+# Movie Model
+class Movie(db.Model):
+    __tablename__ = 'movies'
 
-'''
-Person
-Have title and release year
-'''
-class Person(db.Model):  
-  __tablename__ = 'People'
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    release_date = Column(Date, nullable=False)
 
-  id = Column(db.Integer, primary_key=True)
-  name = Column(String)
-  catchphrase = Column(String)
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
 
-  def __init__(self, name, catchphrase=""):
-    self.name = name
-    self.catchphrase = catchphrase
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date
+        }
 
-  def format(self):
-    return {
-      'id': self.id,
-      'name': self.name,
-      'catchphrase': self.catchphrase}
+# Actor Model
+class Actor(db.Model):
+    __tablename__ = 'actors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    age = Column(Integer, nullable=False)
+    gender = Column(String, nullable=False)
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
