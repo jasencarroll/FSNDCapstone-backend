86fcfdfed9c57c491726417831e2305dc41b1211
Jasen Carroll <108644550+jasencarroll@users.noreply.github.com>
2024-10-22 18:53:43 -0400

    added home page and a means of logging in

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-import os
+from os import environ as env
 from flask import Flask, request, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 from models import setup_db, Movie, Actor
 from flask_cors import CORS
 from flask_migrate import Migrate
 from models import db
-from dotenv import load_dotenv
+import json
+from urllib.parse import quote_plus, urlencode
+from authlib.integrations.flask_client import OAuth
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, redirect, render_template, session, url_for
 
 # Load environment variables from a .env file
-load_dotenv()
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
 
 # Auth0 and RBAC imports
 from auth import requires_auth
@@ -20,6 +26,20 @@ def create_app(test_config=None):
     setup_db(app)
     CORS(app)
     migrate.init_app(app, db)
+    
+    app.secret_key = env.get("APP_SECRET_KEY")
+    
+    oauth = OAuth(app)
+
+    oauth.register(
+        "auth0",
+        client_id=env.get("AUTH0_CLIENT_ID"),
+        client_secret=env.get("AUTH0_CLIENT_SECRET"),
+        client_kwargs={
+            "scope": "openid profile email",
+        },
+        server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+    )
     
     # Ensure that AuthError inherits from Exception
     class AuthError(Exception):
@@ -36,11 +56,36 @@ def create_app(test_config=None):
         return response
     
     # GET /
-    @app.route('/', methods=['GET'])
-    def index():
-        return jsonify({
-            'success': True,
-        }), 200
+    @app.route("/")
+    def home():
+        return render_template("home.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
+
+    @app.route("/login")
+    def login():
+        return oauth.auth0.authorize_redirect(
+            redirect_uri=url_for("callback", _external=True)
+        )
+        
+    @app.route("/callback", methods=["GET", "POST"])
+    def callback():
+        token = oauth.auth0.authorize_access_token()
+        session["user"] = token
+        return redirect("/")
+    
+    @app.route("/logout")
+    def logout():
+        session.clear()
+        return redirect(
+            "https://" + env.get("AUTH0_DOMAIN")
+            + "/v2/logout?"
+            + urlencode(
+                {
+                    "returnTo": url_for("home", _external=True),
+                    "client_id": env.get("AUTH0_CLIENT_ID"),
+                },
+                quote_via=quote_plus,
+            )
+        )
 
     # GET /movies
     @app.route('/movies', methods=['GET'])
