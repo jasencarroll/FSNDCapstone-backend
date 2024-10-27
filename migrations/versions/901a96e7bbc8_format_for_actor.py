651ecc9a4f86731852624754ea540b79331c7ce6
Jasen Carroll <108644550+jasencarroll@users.noreply.github.com>
2024-10-27 00:18:42 +0000

    still working on migrations

--- migrations/versions/901a96e7bbc8_format_for_actor.py
+++ migrations/versions/901a96e7bbc8_format_for_actor.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '901a96e7bbc8'
-down_revision = '9e7df077ad78'
+down_revision = 'previous_revision'
 branch_labels = None
 depends_on = None
 
